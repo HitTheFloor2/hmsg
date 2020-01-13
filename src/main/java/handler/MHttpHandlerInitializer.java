72222c686df5e177be3879ae1efec1a350ac4dd4
Wang <1412215123@qq.com>
2020-01-13 21:01:09 +0800

    move code to MessageManager

---
+++ src/main/java/handler/MHttpHandlerInitializer.java
@@ -0,0 +1,24 @@
+package handler;
+
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelPipeline;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.handler.codec.http.HttpObjectAggregator;
+import io.netty.handler.codec.http.HttpServerCodec;
+import io.netty.handler.codec.http.websocketx.WebSocketServerProtocolHandler;
+import io.netty.handler.stream.ChunkedWriteHandler;
+
+public class MHttpHandler extends ChannelInitializer<SocketChannel> {
+    @Override
+    protected void initChannel(SocketChannel socketChannel) throws Exception {
+        ChannelPipeline pipeline = socketChannel.pipeline();
+        //编解码，大文件，聚合
+        pipeline.addLast(new HttpServerCodec());
+        pipeline.addLast(new ChunkedWriteHandler());
+        pipeline.addLast(new HttpObjectAggregator(1024*64));
+        //路由
+        pipeline.addLast(new WebSocketServerProtocolHandler("/hitty"));
+        //定义
+        
+    }
+}
